ce728251ada9c5646874878f68edaf63ace3ad4c
JonasNon <officialjonasn@gmail.com>
2024-08-30 23:48:45 -0500

    fixed title

--- src/UpperAppBar.js
+++ src/UpperAppBar.js
@@ -14,17 +14,8 @@ export default function UpperAppBar(props) {
     <Box sx={{ flexGrow: 1 }}>
       <AppBar position="static">
         <Toolbar>
-          <IconButton
-            size="large"
-            edge="start"
-            color="inherit"
-            aria-label="menu"
-            sx={{ mr: 2 }}
-          >
-            <MenuIcon />
-          </IconButton>
           <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
-            News
+            My Music App
           </Typography>
           <Button color="inherit" onClick={toggleLogin}>{loggedIn ? "Log Out" : "Login"}</Button>
         </Toolbar>
