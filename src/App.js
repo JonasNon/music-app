e58e54f36d1acc0cf55a60074995646a73338648
JonasNon <officialjonasn@gmail.com>
2024-09-03 18:28:37 -0500

    added cookies for login and username

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import './App.css';
-import React from 'react';
+import React, { useEffect } from 'react';
 import { useState } from 'react';
 import LoginForm from './components/LoginForm';
 import DashBoard from './components/DashBoard';
 import UpperAppBar from './components/UpperAppBar';
+import cookie from "cookie";
 
 function App() {
 
@@ -14,18 +15,38 @@ function App() {
     setUsername(event.target.value)
   }
 
+  const checkIfLoggedIn = () => {
+    if (cookie.parse(document.cookie).loggedIn === "true") {
+      setLoggedIn(true)
+    } else {
+      setLoggedIn(false)
+    }
+  }
+
+  useEffect(() => checkIfLoggedIn(), [])
 
   const toggleLogin = () => {
-    loggedIn ? setLoggedIn(false) : setLoggedIn(true)
-    console.log(loggedIn)
+    if (loggedIn) {
+      setLoggedIn(false)
+      document.cookie = cookie.serialize("loggedIn", "false", { maxAge: 1000 * 60 });
+      document.cookie = cookie.serialize("username", null, { maxAge: 0 });
+
+    } else {
+      setLoggedIn(true)
+      document.cookie = cookie.serialize("loggedIn", "true", { maxAge: 1000 * 60 });
+      document.cookie = cookie.serialize("username", username, { maxAge: 1000 * 60 });
+
+      // console.log(typeof cookie.parse(document.cookie).loggedIn)
+    }
   }
 
   return (
     <div className="App">
-      <UpperAppBar loggedIn={loggedIn} toggleLogin={toggleLogin}></UpperAppBar>
-        {loggedIn ? 
+      {/* <button onClick={() => console.log(cookie.parse(document.cookie).loggedIn)}>test</button> */}
+      <UpperAppBar loggedIn={cookie.parse(document.cookie).loggedIn === "true"} toggleLogin={toggleLogin}></UpperAppBar>
+        {cookie.parse(document.cookie).loggedIn === "true" ? 
         <div> 
-          <DashBoard username={username}></DashBoard>
+          <DashBoard username={cookie.parse(document.cookie).username}></DashBoard>
         </div> :
         <LoginForm username={username} handleUsernameInput={handleUsernameInput} toggleLogin={toggleLogin}></LoginForm>
 
--- src/components/DashBoard.js
+++ src/components/DashBoard.js
@@ -1,10 +1,16 @@
 import React from "react";
 import { useState } from 'react';
 import "./DashBoard.css"
+import cookie from "cookie"
 
 import OnlineCard from './OnlineCard'
 import VolumeCard from './VolumeCard'
 import SoundQualityCard from "./SoundQualityCard";
+
+console.log(cookie.parse(document.cookie));
+// document.cookie = cookie.serialize("loggedIn", "true", { maxAge: 1000 * 60 });
+console.log(cookie.parse(document.cookie));
+
 
 function DashBoard (props) {
   const {username} = props
@@ -24,6 +30,7 @@ function DashBoard (props) {
       toggleNotification("Your application is offline. You won't be able to share or stream music to other devices.")
     } else {
       setIsOffline(true)
+      console.log(cookie.parse(document.cookie))
       toggleNotification("Your application is offline. You won't be able to share or stream music to other devices.")
     }
   }
